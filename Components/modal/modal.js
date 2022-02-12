c4da3a3603f965133fa7fce1616090d63621b2d4
aman-can <aman.dubey1998@gmail.com>
2022-02-13 03:20:00 +0530

    remove dummy iframes and fixed some bugs

--- Components/modal/modal.js
+++ Components/modal/modal.js
@@ -5,6 +5,7 @@ modals.forEach((modal) => {
     modal.classList.add("modal-closed");
     modal.querySelectorAll(".modal-close-btn").forEach((btn) => {
         btn.addEventListener("click", () => {
+            document.querySelector("body").style.overflow = "auto";
             modal.classList.add("modal-closed");
         });
     });
@@ -14,6 +15,7 @@ modalOpeners.forEach((btn) => {
     btn.addEventListener("click", () => {
         modals.forEach((modal) => {
             if (btn.getAttribute("for") === modal.getAttribute("id")) {
+                document.querySelector("body").style.overflow = "hidden";
                 modal.classList.remove("modal-closed");
             }
         });
